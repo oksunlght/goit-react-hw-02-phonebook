381e65f9f00f19ee9cbaa5a70439830c51ab2c65
1Oks3Ana2 <38954729+oksunlght@users.noreply.github.com>
2023-07-29 16:41:45 +0300

    created contact component

--- src/components/Phonebook/ContactList.jsx
+++ src/components/Phonebook/ContactList.jsx
@@ -1,29 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { Item, DeleteBtn } from './Phonebook.styled';
 
-const ContactList = ({ contacts, onDeleteContact }) => (
-  <ul>
-    {contacts.map(({ id, name, number }) => (
-      <Item key={id}>
-        &#8226; {name} {number}
-        <DeleteBtn type="button" onClick={() => onDeleteContact(id)}>
-          Delete
-        </DeleteBtn>
-      </Item>
-    ))}
-  </ul>
-);
+const ContactList = ({ children }) => <ul>{children}</ul>;
 
 export default ContactList;
-
-ContactList.propTypes = {
-  contacts: PropTypes.arrayOf(
-    PropTypes.exact({
-      id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      number: PropTypes.string.isRequired,
-    }).isRequired
-  ).isRequired,
-  onDeleteContact: PropTypes.elementType.isRequired,
-};
